7224c7dddd421de94b3b92b9ad6d1e746666d210
Alexander Green <aeg0322@gmail.com>
2015-11-29 11:54:58 -0500

    save

--- hello/py/hello.py
+++ hello/py/hello.py
@@ -1,5 +1,12 @@
 import time as t
 import skilstak.colors as c
+
+def print_murica(message):
+    """Prints a message in red, white and blue. #Murica"""
+    print(c.cl)
+    while True:
+        print(c.merica() + message, end =" ")
+
 def print_plain(message):
     """Prints Hello World to the Console"""
     print(c.cl + message + c.x)
@@ -21,17 +28,15 @@ def print_forever(message):
 
 def parse_args():
     import sys
-    p = {
-            "who":"world",
-            "option": ""
-        }
+    name = "world"
+    option = ""
     
     if len(sys.argv) > 2:
-       p['option'] = sys.argv[1]
-       p["who"] = sys.argv[2]
+       option = sys.argv[1]
+       who = sys.argv[2]
     elif len(sys.argv) == 2:
         if sys.argv[1].startswith("-"):
-           p['option'] = sys.argv[1]
+           option = sys.argv[1]
         else:
-            p['who'] = sys.argv[1]
-        return p
+            who = sys.argv[1]
+        return name, option
