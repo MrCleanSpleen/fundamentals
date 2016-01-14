f232f88e5a4049f07af6665205377f5e591d7fa7
Alexander Green <aeg0322@gmail.com>
2016-01-13 20:45:51 -0500

    save

--- go/src/hello/main.go
+++ go/src/hello/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	c "github.com/skilstak/go/colors"
+	//i "github.com/skilstak/go/input"
 )
 
+func printRandom() {
+	//Prints "Hello World" to the command line in random color.
+	fmt.Println(c.Clear + c.Rc() + "Hello World" + c.X)
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	//Prints "Hello World" in a random color forever in a loop.
+	for {
+		fmt.Println(c.Rc() + "Hello World!" + c.X)
+	}
+}
+
+func main1() {
+	fmt.Println(c.Clear + "Hello World!" + c.X)
+}
+
+func printMulti() {
+	fmt.Println(c.Clear + c.Multi("Hello World!") + c.X)
 }
