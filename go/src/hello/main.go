7fb633492a39d88fe9b071952c3c3c1453327f97
Alexander Green <aeg0322@gmail.com>
2016-01-18 12:04:13 -0500

    Update main.go

--- go/src/hello/main.go
+++ go/src/hello/main.go
@@ -8,22 +8,22 @@ import (
 
 func printRandom() {
 	//Prints "Hello World" to the command line in random color.
-	fmt.Println(c.Clear + c.Rc() + "Ryan wuz hear" + c.X)
+	fmt.Println(c.Clear + c.Rc() + "Hello World!" + c.X)
 }
 
 func main() {
 	//Prints "Hello World" in a random color forever in a loop.
 	for {
-		fmt.Println(c.Rc() + "Ryan wuz hear" + c.X)
+		fmt.Println(c.Rc() + "Hello World" + c.X)
 	}
 }
 
 func main1() {
 	//Prints "Hello World!" to the command line.
-	fmt.Println(c.Clear + "Ryan wuz hear" + c.X)
+	fmt.Println(c.Clear + "Hello World" + c.X)
 }
 
 func printMulti() {
-	//Prints
-	fmt.Println(c.Clear + c.Multi("Ryan wuz hear") + c.X)
+	//Prints "Hello World!" To the command line, with each character a random color.
+	fmt.Println(c.Clear + c.Multi("Hello World") + c.X)
 }
