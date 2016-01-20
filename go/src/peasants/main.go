a9bb555671efb4163b08bd77fc35f3250d7638b3
Alexander Green <aeg0322@gmail.com>
2016-01-20 06:52:22 -0500

    save

---
+++ go/src/peasants/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	c "github.com/skilstak/go/colors"
+	i "github.com/skilstak/go/input"
+)
+
+func main() {
+	fmt.Println("I AM A PROGRAMMER!")
+	i.Ask("ARE YOU?")
+}
