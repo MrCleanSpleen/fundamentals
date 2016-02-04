b2ca4f62cc5d2b3fbdf01ec304299d1f86155fc3
Alexander Green <aeg0322@gmail.com>
2016-02-03 20:47:35 -0500

    save

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -3,39 +3,46 @@ package main
 import (
 	"fmt"
 	c "github.com/skilstak/go/colors"
-	//"strings"
+	s "strings"
 	//i "github.com/skilstak/go/input"
 	"os"
+	t "time"
 )
 
-func printPlain() {
+func printPlain(message string) {
 	//Prints "Hello World!" to the command line.
 	fmt.Println(c.Clear + "Hello World" + c.X)
 }
-func printRandom() {
+func printRandom(message string) {
 	//Prints "Hello World" to the command line in random color.
-	fmt.Println(c.Clear + c.Rc() + "Hello World!" + c.X)
+	fmt.Println(c.Clear + c.Rc() + "Hello " + message + c.X)
 }
-
-func printForevah() {
+func printMulti(message string) {
+	//Prints Hello World to the command line with each character a different color.
+	for {
+		fmt.Println(c.Multi("Hello " + message + c.Clear))
+		t.Sleep(500 * t.Milliseconds)
+	}
+}
+func printForevah(message string) {
 	//Prints "Hello World" in a random color forever in a loop.
 	for {
-		fmt.Println(c.Rc() + "Hello World" + c.X)
+		fmt.Print(c.Rc() + "Hello " + message + c.X)
 	}
 }
 
-func main() {
+func parseArgs() {
+	//Parses arguments from the command line
 	name := "YOU"
-	if len(os.Args) > 1 {
+	option := ""
+	if len(os.Args) > 2 {
+		if s.HasPrefix(os.Args[1], "-") {
+			option = os.Args[1]
+			name = os.Args[2]
+		}
+	}
+	if len(os.Args) == 2 {
 		name = os.Args[1]
 	}
-	printMulti("Hello " + name)
-}
-func parseArgs() {
 
-}
-func getName() string {
-	//Gives the var "name" a value and returns it.
-	name := "Alexander"
-	return name
 }
